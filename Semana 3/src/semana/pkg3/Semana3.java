9fe9adb89b742e4bec843ff33013ea63859f5882
Geanca3062 <138180363+Geanca3062@users.noreply.github.com>
2024-01-31 19:00:16 -0600

    Add files via upload

---
+++ Semana 3/src/semana/pkg3/Semana3.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package semana.pkg3;
+
+/**
+ *
+ * @author Aulas Heredia
+ */
+public class Semana3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*for (int x = 10; x >= 0; x--){
+            System.out.println(x);
+        }
+        
+        
+        int i = 10;
+        while (i != 0){
+            System.out.println(i);
+            i--;
+        }
+
+        int x = 10;
+        do {
+            System.out.println(x);
+            x--;
+        }while(x >= 0);
+*/
+        for (int x = 0; x < 5; x++){
+            System.out.println("@@@@@@@@@@");
+        }
+    }
+    
+        
+        
+    
+}
